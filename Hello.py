c2b1d1eb5ded16bfd3752e8b95ea40b75dc72793
Evyatar <evyatar@tikalk.com>
2020-06-23 11:10:30 +0300

    add some hello examples

--- Hello.py
+++ Hello.py
@@ -2,9 +2,24 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
    return 'Hello World'
+
+
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
+
+
+@app.route('/blog/<int:postID>')
+def show_blog(postID):
+   return 'Blog Number %d' % postID
+
+@app.route('/user/<name>')
+def hello_user(name):
+    return hello_name(name)
+
 
 if __name__ == '__main__':
    app.run(debug=True)
